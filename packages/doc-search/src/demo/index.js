f3d5e1f9e846778310468f119161f4028182a33a
zuozhiheng <zuozhiheng@bytedance.com>
2022-08-03 16:23:13 +0800

    doc: update team site

--- packages/doc-search/src/demo/index.js
+++ packages/doc-search/src/demo/index.js
@@ -1,14 +1,12 @@
 /**
  * @file
- * @title 组件名
- * @memberOf 组件类型，例如：数据输入
- * @description 描述你的组件。
- * @author 物料作者
+ * @title DocSearch
+ * @memberOf 信息展示
+ * @description 用于文档站点搜索的弹出框。
  * @package @arco-materials/doc-search
  */
 
 /**
  * @title 基本用法
- * @description 描述你的例子
  */
 export { default as Basic } from './basic';
